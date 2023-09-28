a69bbc31630724212dd4aa1930752896a90a5953
SahydiAbrahao~ <sahydi@protonmail.com>
2023-09-28 10:27:31 -0400

    feat: ensure SignUpController calls validation with correct values

--- src/presentation/signup-controller.ts
+++ src/presentation/signup-controller.ts
@@ -1,11 +1,18 @@
 import { type ICreateAccount } from '@/domain/i-create-account'
 import { type IController, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
+import { type IValidation } from '@/utils/i-validation'
 
 export class SignUpController implements IController {
-  constructor (private readonly createAccount: ICreateAccount) {}
+  constructor (
+    private readonly createAccount: ICreateAccount,
+    private readonly validation: IValidation) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { name, email, password } = httpRequest.body
+
+      this.validation.validate({ name, email, password })
+
       const account = await this.createAccount.create({ name, email, password })
       return {
         statusCode: 200,
